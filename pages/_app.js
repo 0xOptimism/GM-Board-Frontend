854ee4600eb9107e97baea96a30805589b38ecf8
Yannis <yannis182@outlook.fr>
2022-03-26 13:30:53 +0100

    started the project and built header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "wagmi";
+
+import { theme } from "../theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
