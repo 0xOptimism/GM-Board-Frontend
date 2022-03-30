447d07845ff273a97eab6a0f67bd3bd786e5eb19
Yannis <yannis182@outlook.fr>
2022-03-30 19:43:34 +0200

    added updated contract and fixed winner from the contract

--- store/web3Store.js
+++ store/web3Store.js
@@ -2,7 +2,9 @@ import create from "zustand";
 
 const useWeb3Store = create((set) => ({
   messages: [],
+  totalGms: null,
   setMessages: (message) => set(() => ({ messages: message })),
+  setTotalGms: (data) => set(() => ({ totalGms: data })),
 }));
 
 export default useWeb3Store;
